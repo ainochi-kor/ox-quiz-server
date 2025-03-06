51feaf0603b65ddf181b8a1238a5428c0859be42
ainochi-kor <kthrkdals@gmail.com>
2025-03-06 13:02:01 +0900

    fix: add @Param('id') in getQuizById method

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,5 +1,5 @@
 // src/controllers/quiz.controller.ts
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body, Param } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { CreateQuizDto } from 'src/dtos/create-quiz.dto/create-quiz.dto';
 
@@ -18,7 +18,8 @@ export class QuizController {
   }
 
   @Get(':id')
-  getQuizById(id: string) {
+  getQuizById(@Param('id') id: string) {
+    console.log('Controller getQuizById', id);
     return this.quizService.getQuizById(id);
   }
 }
